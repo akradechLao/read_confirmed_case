0806e029957a786e2c310e631fa5d297be506cae
AkradechL99 <akradechlao1975@gmail.com>
2025-06-25 13:00:42 +0700

    first commit

---
+++ read_confirmed_case.py
@@ -0,0 +1,13 @@
+def read_data(file_name):
+    with open(file_name, mode='r', encoding='utf-8-sig') as f:
+        data = f.readlines()
+    return data 
+
+data = read_data('C:\Users\AkradechNTC\Downloads\confirmed-cases-since-120864.csv')
+keys = data.pop(0).strip().split(',')
+    
+print(keys)
+
+
+
+
